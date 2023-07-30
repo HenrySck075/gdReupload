0902d6264427e8636681b4af7416d8153a758713
HenrySck075 <email.is.not.vaild9@gmail.com>
2023-07-30 10:16:06 +0700

    abandon pyscript version

--- server.py
+++ server.py
@@ -42,7 +42,6 @@ def encode_gjp(password: str) -> str:
     return encoded_base64
 
 def resp2json(resp, fields=[]):
-    print(resp)
     resp= resp.split("#")[0] #a
     r = re.compile("[^:]+:[^:]+")
     ret = {}
@@ -75,6 +74,7 @@ def generate_rs(n=10):
 def reuplaod():
     if request.method == "GET": return "hi"
     if request.method == "POST":
+        ip = request.remote_addr
         target = request.json["target"]
         dest = request.json["dest"]
         levelId = request.json["levelId"]
@@ -93,12 +93,13 @@ def reuplaod():
             if (target[0] not in gdpsMap or "https" in gdpsMap[target[0]]) or (dest[0] not in gdpsMap or "https" in gdpsMap[dest[0]]):
                 setError("Server not supported or not exist")
             else:
+                header = {"User-Agent": "", "x-forwarded-for": ip, "x-real-ip": ip}
                 # downloadGJLevel22.php now requires rs and chk key so yeah that's my rs and chk
-                accId = resp2json(requests.post(gdpsMap[dest[0]]+"getGJUsers20.php", data={"secret": "Wmfd2893gb7", "str":dest[1],"total": "0", "page":"0"}, headers={"User-Agent": ""}).text, ["16"]),
-                accIdTarget = resp2json(requests.post(gdpsMap[target[0]]+"getGJUsers20.php", data={"secret": "Wmfd2893gb7", "str":target[1],"total": "0", "page":"0"}, headers={"User-Agent": ""}).text, ["16"]),
+                accId = resp2json(requests.post(gdpsMap[dest[0]]+"getGJUsers20.php", data={"secret": "Wmfd2893gb7", "str":dest[1],"total": "0", "page":"0"}, headers=header).text, ["16"]),
+                accIdTarget = resp2json(requests.post(gdpsMap[target[0]]+"getGJUsers20.php", data={"secret": "Wmfd2893gb7", "str":target[1],"total": "0", "page":"0"}, headers=header).text, ["16"]),
                 rs=generate_rs()
                 body2={"gameVersion": "21", "binaryVersion": "35","gdw":"0", "accountID":accIdTarget[0]["16"],"gjp": encode_gjp(target[2]), "uuid":"S1521388267807637760849071701082101002","udid":"147699869","levelID": levelId, "inc":"1", "extras":"0", "secret": "Wmfd2893gb7", "rs": rs, "chk": generate_chk([levelId, "1", rs, accIdTarget[0]["16"], "S1521388267807637760849071701082101002", "147699869"], "41274", "xI25fpAapCQg")}
-                resp = resp2json(requests.post(gdpsMap[target[0]]+"downloadGJLevel22.php", data={k:body2[k] for k in body2 if k in dgjlParams}, headers={"User-Agent": ""}).text)
+                resp = resp2json(requests.post(gdpsMap[target[0]]+"downloadGJLevel22.php", data={k:body2[k] for k in body2 if k in dgjlParams}, headers=header).text)
                 upload = requests.post(gdpsMap[dest[0]]+"uploadGJLevel21.php", {
                     "gameVersion": 21,
                     "accountID": int(accId[0]["16"]),
@@ -123,7 +124,7 @@ def reuplaod():
                     "levelString": resp["4"]+"="*(-len(resp["4"])%4),
                     "seed2": generate_chk(key="41274", values=[generate_upload_seed(resp["4"])], salt="xI25fpAapCQg"),
                     "secret": "Wmfd2893gb7"
-                })
+                }, headers=header)
                 body = {"resp":"Level ID: "+upload.text}
 
         except Exception as e:
